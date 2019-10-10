051702acce58da8a8b7ab218c8b9435bfaedfc2f
isx47797439 <isx47797439@i08.informatica.escoladeltreball.org>
2019-10-10 12:09:02 +0200

    correccions ldapserver19:multi

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 ## @slagtand Marc Gómez ASIX M06-ASO Curs 2019-2020
 Repo per a m06 19-20 de ldapserver
 * **ldapserver:base** servidor ldap bàsic
+* **ldapserver:multi** servidor amb dues bases de dades, dc=edt,dc=org i dc=m06,dc=cat
--- ldapserver19:multi/install.sh
+++ ldapserver19:multi/install.sh
@@ -2,10 +2,15 @@
 # Install ldap server
 rm -rf /etc/openldap/slapd.d/*
 rm -rf /var/lib/ldap/*
-cp /opt/docker/DB_CONFIG /var/lib/ldap/.
-slaptest -f /opt/docker/slapd-edt.org.conf -F /etc/openldap/slapd.d
-slapadd -F /etc/openldap/slapd.d -l /opt/docker/organitzacio_edt.org.ldif
-slapadd -F /etc/openldap/slapd.d -l /opt/docker/usuaris_edt_org.ldif
+mkdir /var/lib/ldap.edt.org
+mkdir /var/lib/ldap.m06.cat
+cp /opt/docker/DB_CONFIG /var/lib/ldap.edt.org
+cp /opt/docker/DB_CONFIG /var/lib/ldap.m06.cat
+slaptest -f /opt/docker/slapd.conf -F /etc/openldap/slapd.d
+slapadd -F /etc/openldap/slapd.d -l /opt/docker/edt.org.ldif
+slapadd -b 'dc=m06,dc=cat' -F /etc/openldap/slapd.d -l /opt/docker/m06.cat.ldif
 chown -R ldap.ldap /etc/openldap/slapd.d/
 chown -R ldap.ldap /var/lib/ldap/
+chown -R ldap.ldap /var/lib/ldap.edt.org/
+chown -R ldap.ldap /var/lib/ldap.m06.cat/
 cp /opt/docker/ldap.conf /etc/openldap/.
